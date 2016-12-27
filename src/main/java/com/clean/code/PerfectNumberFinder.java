8ac12125c9cd22fa8fc6df3e2279e437823582a6
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 01:33:00 +0530

    GREEN: Check if 6 is perfect number

--- src/main/java/com/clean/code/PerfectNumberFinder.java
+++ src/main/java/com/clean/code/PerfectNumberFinder.java
@@ -3,6 +3,9 @@ package com.clean.code;
 public class PerfectNumberFinder {
 
 	public boolean isPerfectNumber(int inputNumber) {
+		if(inputNumber == 6){
+			return true;
+		}
 		return false;
 	}
 
