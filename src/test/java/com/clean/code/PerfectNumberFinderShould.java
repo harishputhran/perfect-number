fb59375ac7cf24a41022903a1683e5a942d74d6d
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 01:32:04 +0530

    RED: Check if 6 is perfect number

--- src/test/java/com/clean/code/PerfectNumberFinderShould.java
+++ src/test/java/com/clean/code/PerfectNumberFinderShould.java
@@ -7,9 +7,17 @@ import org.junit.Test;
 public class PerfectNumberFinderShould {
 
 	@Test
-	public void return_False_when_input_number_is_1() {
+	public void return_False_when_input_number_is_1_as_it_is_not_a_Perfect_Number() {
 		PerfectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
 		assertFalse(perfectNumberFinder.isPerfectNumber(1));
 	}
+	
+	@Test
+	public void return_True_when_input_number_is_6_as_it_is_a_Perfect_Number() {
+		PerfectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
+		assertTrue(perfectNumberFinder.isPerfectNumber(6));
+	}
+	
+	
 
 }
