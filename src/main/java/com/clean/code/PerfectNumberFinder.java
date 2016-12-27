23482e4982fc2ad0a13947b9da087b4675975225
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 01:59:34 +0530

    REFACTOR: Sum of Divisors is determined in a different method.

--- src/main/java/com/clean/code/PerfectNumberFinder.java
+++ src/main/java/com/clean/code/PerfectNumberFinder.java
@@ -3,14 +3,22 @@ package com.clean.code;
 public class PerfectNumberFinder {
 
 	public boolean isPerfectNumber(int inputNumber) {
+		return sumOfDivisors(inputNumber) == inputNumber;
+	}
+
+	protected int sumOfDivisors(int inputNumber) {
 		int divisor = 1;
 		int sumOfDivisors = 0;
-		while(inputNumber  > divisor){
-			if(inputNumber % divisor == 0){
-				sumOfDivisors=+inputNumber;
+		while(inputNumber > divisor){
+			if(isDivisible(inputNumber, divisor)){
+				sumOfDivisors = sumOfDivisors + divisor;
 			}
 			divisor++;
 		}
-		return sumOfDivisors == inputNumber ? Boolean.TRUE : Boolean.FALSE;
+		return sumOfDivisors;
+	}
+
+	protected boolean isDivisible(int inputNumber, int divisor) {
+		return inputNumber % divisor == 0;
 	}
 }
--- src/test/java/com/clean/code/PerfectNumberFinderShould.java
+++ src/test/java/com/clean/code/PerfectNumberFinderShould.java
@@ -2,28 +2,31 @@ package com.clean.code;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PerfectNumberFinderShould {
+	
+	private PerfectNumberFinder perfectNumberFinder;
+	
+	@Before
+	public void setUp() {
+		perfectNumberFinder = new PerfectNumberFinder();
+	}
+	
 
 	@Test
 	public void return_False_when_input_number_is_1_as_it_is_not_a_Perfect_Number() {
-		PerfectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
 		assertFalse(perfectNumberFinder.isPerfectNumber(1));
 	}
 	
 	@Test
 	public void return_True_when_input_number_is_6_as_it_is_a_Perfect_Number() {
-		PerfectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
 		assertTrue(perfectNumberFinder.isPerfectNumber(6));
 	}
 	
 	@Test
 	public void return_True_when_input_number_is_28_as_it_is_a_Perfect_Number() {
-		PerfectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
 		assertTrue(perfectNumberFinder.isPerfectNumber(28));
 	}
-	
-	
-
 }
