cdb215189140beaa7e9b1367f4c6a6d73fff0190
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-28 01:36:42 +0530

    GREEN: Check if 28 is perfect number

--- src/main/java/com/clean/code/PerfectNumberFinder.java
+++ src/main/java/com/clean/code/PerfectNumberFinder.java
@@ -3,7 +3,7 @@ package com.clean.code;
 public class PerfectNumberFinder {
 
 	public boolean isPerfectNumber(int inputNumber) {
-		if(inputNumber == 6){
+		if(inputNumber == 6 || inputNumber ==28){
 			return true;
 		}
 		return false;
